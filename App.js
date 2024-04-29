74b68517e54ff5f626ba16c0cd75a2ed7d3a2c81
adamhinton <adam.d.hinton@gmail.com>
2024-04-29 12:47:10 -0400

    config

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        boopbeep Hi, I just completed my first React Native Assignment!
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
