31dfc1107ad26077070eb776561a72196ee22d4e
adamhinton <adam.d.hinton@gmail.com>
2024-04-30 11:59:37 -0400

    my first TextInput

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
 import {SafeAreaView, Text, TextInput} from 'react-native';
 
 const App = () => {
+  const [textValue, setTextValue] = useState('');
+
   return (
     <SafeAreaView>
-      <TextInput />
+      <TextInput
+        style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+        value={textValue}
+        onChangeText={setTextValue}
+      />
     </SafeAreaView>
   );
 };
