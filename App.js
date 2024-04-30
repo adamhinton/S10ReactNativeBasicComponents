23df61982fba2a29a3e25a4a47f5564604bb056f
adamhinton <adam.d.hinton@gmail.com>
2024-04-30 12:07:00 -0400

    keyboardType

--- App.js
+++ App.js
@@ -4,13 +4,30 @@ import {SafeAreaView, Text, TextInput} from 'react-native';
 
 const App = () => {
   const [textValue, setTextValue] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <SafeAreaView>
       <TextInput
         style={{borderWidth: 1, padding: 10, borderRadius: 4}}
         value={textValue}
-        onChangeText={setTextValue}
+        onChangeText={value => {
+          setTextValue(value);
+        }}
+        autoFocus={true}
+        placeholder="Please enter your name"
+      />
+
+      <TextInput
+        style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+        value={password}
+        onChangeText={value => {
+          setPassword(value);
+        }}
+        secureTextEntry={true}
+        autoFocus={true}
+        placeholder="Password"
+        keyboardType="default"
       />
     </SafeAreaView>
   );
