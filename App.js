622fa8dd0693df7adcc0a74c8bcef3d1e3ad8357
adamhinton <adam.d.hinton@gmail.com>
2024-04-30 21:55:51 -0400

    Prep for Button lecture

--- App.js
+++ App.js
@@ -4,27 +4,7 @@ import {Image, SafeAreaView, ScrollView, Text, TextInput} from 'react-native';
 const App = () => {
   return (
     <SafeAreaView>
-      <ScrollView
-        onScroll={() => {
-          console.log('We are now scrolling');
-        }}
-        contentContainerStyle={{backgroundColor: 'red', height: 600}}
-        showsVerticalScrollIndicator={false}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}>
-        <Image
-          source={require('./assets/images/cake.png')}
-          style={{height: 500, width: 500}}
-        />
-        <Image
-          source={require('./assets/images/cake.png')}
-          style={{height: 500, width: 500}}
-        />
-        <Image
-          source={require('./assets/images/cake.png')}
-          style={{height: 500, width: 500}}
-        />
-      </ScrollView>
+      <ScrollView />
     </SafeAreaView>
   );
 };
