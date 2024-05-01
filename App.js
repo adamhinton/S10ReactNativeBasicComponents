009cea302459a2e249a16fa8c4a86e6827c2550e
adamhinton <adam.d.hinton@gmail.com>
2024-04-30 21:53:09 -0400

    My first React Native ScrollView

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Text, TextInput} from 'react-native';
+import {Image, SafeAreaView, ScrollView, Text, TextInput} from 'react-native';
 
 const App = () => {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <ScrollView
+        onScroll={() => {
+          console.log('We are now scrolling');
+        }}
+        contentContainerStyle={{backgroundColor: 'red', height: 600}}
+        showsVerticalScrollIndicator={false}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}>
+        <Image
+          source={require('./assets/images/cake.png')}
+          style={{height: 500, width: 500}}
+        />
+        <Image
+          source={require('./assets/images/cake.png')}
+          style={{height: 500, width: 500}}
+        />
+        <Image
+          source={require('./assets/images/cake.png')}
+          style={{height: 500, width: 500}}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default App;
