47000901ade98e25a71bb5975c9be64cb4392f43
adamhinton <adam.d.hinton@gmail.com>
2024-05-02 13:18:42 -0400

    flexbox experimentation

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import {View} from 'react-native';
 
 const App = () => {
-  return null;
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'red',
+        flexDirection: 'column-reverse',
+      }}>
+      <View
+        style={{flex: 1, backgroundColor: 'yellow', width: 200, height: 200}}
+      />
+      <View
+        style={{flex: 2, backgroundColor: 'green', width: 200, height: 200}}
+      />
+      <View
+        style={{flex: 3, backgroundColor: 'black', width: 200, height: 200}}
+      />
+    </View>
+  );
 };
 
 export default App;
